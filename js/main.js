ddf9860e3609c43c36446e4fdb43748f9208af5b
Gregor Gatej <gregor.gatej@gmail.com>
2025-12-15 20:43:54 +0100

    feat: add basic math operations

---
+++ js/main.js
@@ -0,0 +1,37 @@
+function add(num1, num2) {
+    return num1 + num2;
+}
+
+function subtract(num1, num2) {
+    return num1 - num2;
+}
+
+function multiply(num1, num2) {
+    return num1 * num2;
+}
+
+function divide(num1, num2) {
+    return num1 / num2;
+}
+
+let num1;
+let operator;
+let num2;
+
+function operate (operator, num1, num2) {
+    switch(operator) {
+        case '+':
+            return add(num1, num2);
+        case '-':
+            return subtract(num1, num2);
+        case '*':
+            return multiply(num1, num2);
+        case '/':
+            if (num2 === 0) {
+                throw new Error("Division by zero");
+            }
+            return divide(num1, num2);
+        default:
+            throw new Error(`Unknown operator: ${operator}`);
+    }
+}
